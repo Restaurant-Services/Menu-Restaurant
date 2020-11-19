4d771c57b88757ee281c273400fb4152e8374aea
Pelat-1 <nicolo.rogai@outlook.com>
2020-11-19 17:11:24 +0100

    Credits Done

--- src/app/credits/credits.component.ts
+++ src/app/credits/credits.component.ts
@@ -1,16 +1,24 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+import { MatIconRegistry } from '@angular/material/icon';
+import { DomSanitizer } from '@angular/platform-browser';
+import {RecipeType} from '../shared/recipe-type';
+import {RECIPE_TYPES} from '../shared/mock/mock-recipe-type';
 
 @Component({
   selector: 'app-credits',
   templateUrl: './credits.component.html',
   styleUrls: ['./credits.component.css']
 })
-export class CreditsComponent implements OnInit {
+export class CreditsComponent {
+  readonly recipeTypes: RecipeType[] = RECIPE_TYPES;
 
-  constructor() {
+  constructor(private matIconRegistry: MatIconRegistry,
+              private domSanitizer: DomSanitizer) {
+    for (const type of this.recipeTypes) {
+      this.matIconRegistry.addSvgIcon(
+        type.svg,
+        this.domSanitizer.bypassSecurityTrustResourceUrl(type.getSVGPathFromComponent())
+      );
+    }
   }
-
-  ngOnInit(): void {
-  }
-
 }
