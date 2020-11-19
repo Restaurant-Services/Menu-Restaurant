b6bf610463ced56b8286c5ff144da6056e12ed32
Pelat-1 <nicolo.rogai@outlook.com>
2020-11-19 17:09:02 +0100

    Sidenav works

--- src/app/sidenav/sidenav.component.ts
+++ src/app/sidenav/sidenav.component.ts
@@ -1,15 +1,39 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+import {MatIconRegistry} from '@angular/material/icon';
+import {DomSanitizer} from '@angular/platform-browser';
+import {RecipeType} from '../shared/recipe-type';
+import {RECIPE_TYPES} from '../shared/mock/mock-recipe-type';
 
 @Component({
   selector: 'app-sidenav',
   templateUrl: './sidenav.component.html',
   styleUrls: ['./sidenav.component.css']
 })
-export class SidenavComponent implements OnInit {
+export class SidenavComponent {
+  readonly recipeTypes: RecipeType[] = RECIPE_TYPES;
 
-  constructor() { }
+  showSubmenu = false;
+  isExpanded = false;
+  isShowing = false;
 
-  ngOnInit(): void {
+  constructor(private matIconRegistry: MatIconRegistry,
+              private domSanitizer: DomSanitizer) {
+    for (const type of this.recipeTypes) {
+      this.matIconRegistry.addSvgIcon(
+        type.svg,
+        this.domSanitizer.bypassSecurityTrustResourceUrl(type.getSVGPathFromComponent())
+      );
+    }
   }
 
+  mouseenter(): void {
+    if (!this.isExpanded) {
+      this.isShowing = true;
+    }
+  }
+  mouseleave(): void {
+    if (!this.isExpanded) {
+      this.isShowing = false;
+    }
+  }
 }
