9a16b400e3de042ab80cd5e182d669b3be76eaf3
Pelat-1 <nicolo.rogai@outlook.com>
2020-11-20 16:57:54 +0100

    Mock Ingredients, at the momento only optional ingredients

---
+++ src/app/shared/mock/mock-ingredients.ts
@@ -0,0 +1,19 @@
+import { Ingredient } from '../ingredient';
+
+export const OPTIONAL_INGREDIENTS: Ingredient[] = [
+  { id: 0, name: 'Wasabi'},
+  { id: 1, name: 'Maionese'},
+  { id: 2, name: 'Ketchup'},
+  { id: 3, name: 'Tabasco'},
+  { id: 4, name: 'Zenzero'},
+  { id: 5, name: 'Arachidi'},
+  { id: 6, name: 'Salsa Teriyaki'},
+  { id: 7, name: 'Philadelphia'},
+  { id: 8, name: 'Cipolle Fritte'},
+  { id: 9, name: 'Mandorle'},
+  { id: 10, name: 'Ikura'},
+  { id: 11, name: 'Cetriolo'},
+  { id: 12, name: 'Sesamo'},
+  { id: 13, name: 'Avocado'},
+  { id: 14, name: 'Panna'},
+];
