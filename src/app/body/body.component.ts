c85068e7cebc9e3a7463c34db10f1d81f27d04c9
Pelat-1 <nicolo.rogai@outlook.com>
2020-11-20 16:23:40 +0100

    Body and Credits

--- src/app/body/body.component.ts
+++ src/app/body/body.component.ts
@@ -1,15 +1,14 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, Input, OnInit} from '@angular/core';
+import {RecipeType} from '../shared/recipe-type';
 
 @Component({
   selector: 'app-body',
   templateUrl: './body.component.html',
   styleUrls: ['./body.component.css']
 })
-export class BodyComponent implements OnInit {
+export class BodyComponent {
+  @Input()
+  recipeType: RecipeType;
 
   constructor() { }
-
-  ngOnInit(): void {
-  }
-
 }
--- src/app/credits/credits.component.ts
+++ src/app/credits/credits.component.ts
@@ -1,6 +1,6 @@
 import { Component } from '@angular/core';
 import { MatIconRegistry } from '@angular/material/icon';
-import { DomSanitizer } from '@angular/platform-browser';
+import {DomSanitizer, SafeResourceUrl} from '@angular/platform-browser';
 import {RecipeType} from '../shared/recipe-type';
 import {RECIPE_TYPES} from '../shared/mock/mock-recipe-type';
 
@@ -10,15 +10,16 @@ import {RECIPE_TYPES} from '../shared/mock/mock-recipe-type';
   styleUrls: ['./credits.component.css']
 })
 export class CreditsComponent {
-  readonly recipeTypes: RecipeType[] = RECIPE_TYPES;
+  readonly recipeTypes: RecipeType[] = RECIPE_TYPES.sort((a, b) => {
+    return a.id - b.id;
+  }).slice(2);
 
   constructor(private matIconRegistry: MatIconRegistry,
               private domSanitizer: DomSanitizer) {
     for (const type of this.recipeTypes) {
-      this.matIconRegistry.addSvgIcon(
-        type.svg,
-        this.domSanitizer.bypassSecurityTrustResourceUrl(type.getSVGPathFromComponent())
-      );
+      const svg: string = type.getSVGPathFromComponent();
+      const safeUrl: SafeResourceUrl = this.domSanitizer.bypassSecurityTrustResourceUrl(svg);
+      this.matIconRegistry.addSvgIcon(type.svg, safeUrl);
     }
   }
 }
