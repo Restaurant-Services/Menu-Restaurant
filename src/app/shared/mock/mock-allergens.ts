b46d1d275cd6ef32f201b7e5d057f6218984ae5f
Pelat-1 <nicolo.rogai@outlook.com>
2020-11-20 17:06:09 +0100

    Allergens and it's mock

---
+++ src/app/shared/mock/mock-allergens.ts
@@ -0,0 +1,19 @@
+import { Allergen } from '../allergen';
+
+export const ALLERGENS: Allergen[] = [
+  { id: 0, name: 'Verdure' },
+  { id: 1, name: 'Glutine' },
+  { id: 2, name: 'Pesce Surgelato' },
+  { id: 3, name: 'Soia' },
+  { id: 4, name: 'Crostacei' },
+  { id: 5, name: 'Frutta e Guscio' },
+  { id: 6, name: 'Uova' },
+  { id: 7, name: 'Maiale' },
+  { id: 8, name: 'Piccante' },
+  { id: 9, name: 'Pesce' },
+  { id: 10, name: 'Latticini' },
+  { id: 11, name: 'Surgelato' },
+  { id: 12, name: 'Abbattuto' },
+  { id: 13, name: 'Vegetariano'},
+  { id: 14, name: 'Vegeno'},
+];
