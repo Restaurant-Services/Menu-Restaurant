f6dab2bb32ef3f85f9397128a28b1ef78a48a46f
Pelat-1 <nicolo.rogai@outlook.com>
2020-11-25 23:43:27 +0100

    Mock fixes

--- src/app/shared/mock/mock-authors.ts
+++ src/app/shared/mock/mock-authors.ts
@@ -4,4 +4,6 @@ export const AUTHORS: Author[] = [
   { id: 0, name: 'Eucalyp' },
   { id: 1, name: 'Freepik' },
   { id: 2, name: 'mangsaabguru' }
-];
+].sort((a: Author, b: Author) => {
+  return a.id - b.id;
+});
