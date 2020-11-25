aa51113969449826057357f3225cd2862837e8b2
Pelat-1 <nicolo.rogai@outlook.com>
2020-11-25 12:13:02 +0100

    Fix mock allergens

--- src/app/shared/mock/mock-allergens.ts
+++ src/app/shared/mock/mock-allergens.ts
@@ -1,19 +1,22 @@
 import { Allergen } from '../allergen';
 
-export const ALLERGENS: Allergen[] = [
-  { id: 0, name: 'Verdure' },
-  { id: 1, name: 'Glutine' },
-  { id: 2, name: 'Pesce Surgelato' },
-  { id: 3, name: 'Soia' },
-  { id: 4, name: 'Crostacei' },
-  { id: 5, name: 'Frutta e Guscio' },
+export const ALLERGENS_NOTES: Allergen[] = [
+  { id: 0, name: 'Vegen'},
+  { id: 1, name: 'Vegetariano'},
+  { id: 2, name: 'Verdure' },
+  { id: 3, name: 'Glutine' },
+  { id: 4, name: 'Soia' },
+  { id: 5, name: 'Piccante' },
   { id: 6, name: 'Uova' },
-  { id: 7, name: 'Maiale' },
-  { id: 8, name: 'Piccante' },
-  { id: 9, name: 'Pesce' },
-  { id: 10, name: 'Latticini' },
-  { id: 11, name: 'Surgelato' },
-  { id: 12, name: 'Abbattuto' },
-  { id: 13, name: 'Vegetariano'},
-  { id: 14, name: 'Vegeno'},
-];
+  { id: 7, name: 'Frutta e Guscio' },
+  { id: 8, name: 'Mais' },
+  { id: 9, name: 'Latticini' },
+  { id: 10, name: 'Maiale' },
+  { id: 11, name: 'Crostacei' },
+  { id: 12, name: 'Pesce' },
+  { id: 13, name: 'Pesce Surgelato' },
+  { id: 14, name: 'Abbattuto' },
+  { id: 15, name: 'Surgelato' }
+].sort((a: Allergen, b: Allergen) => {
+  return a.id - b.id;
+});
