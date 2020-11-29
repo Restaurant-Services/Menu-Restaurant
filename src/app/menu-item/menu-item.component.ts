0bb4717594327b3492f90f407839bfdf65b404cc
Pelat-1 <nicolo.rogai@outlook.com>
2020-11-29 02:52:21 +0100

    Set emit and fix html

--- src/app/menu-item/menu-item.component.ts
+++ src/app/menu-item/menu-item.component.ts
@@ -1,5 +1,6 @@
-import { Component, Input } from '@angular/core';
+import { Component, EventEmitter, Input, Output } from '@angular/core';
 import { Recipe } from '../shared/recipe';
+import { Order } from '../shared/order';
 
 @Component({
   selector: 'app-menu-item',
@@ -11,15 +12,22 @@ export class MenuItemComponent {
   menuItem: Recipe;
 
   @Input()
-  qty = 0;
+  qty: number;
+
+  @Output()
+  ordering: EventEmitter<Order> = new EventEmitter();
 
   plus1(): void {
-    this.qty += 1;
+    this.emit(new Order(this.menuItem, 1));
   }
 
   minus1(): void {
     if (this.qty > 0) {
-      this.qty -= 1;
+      this.emit(new Order(this.menuItem, -1));
     }
+  }
+
+  private emit(order: Order): void {
+    this.ordering.emit(order);
   }
 }
