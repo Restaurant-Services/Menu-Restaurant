1db3f056cd686a772d88061424f75073395943bc
Pelat-1 <nicolo.rogai@outlook.com>
2020-11-30 16:46:24 +0100

    Modal works

--- src/app/modal/modal.component.ts
+++ src/app/modal/modal.component.ts
@@ -1,15 +1,26 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Inject } from '@angular/core';
+import { MAT_DIALOG_DATA, MatDialogRef } from '@angular/material/dialog';
+import { Order } from '../shared/order';
+import { Recipe } from '../shared/recipe';
 
 @Component({
   selector: 'app-modal',
   templateUrl: './modal.component.html',
   styleUrls: ['./modal.component.css']
 })
-export class ModalComponent implements OnInit {
+export class ModalComponent {
+  order: Order;
 
-  constructor() { }
-
-  ngOnInit(): void {
+  constructor(public dialogRef: MatDialogRef<ModalComponent>,
+              @Inject(MAT_DIALOG_DATA) public data: Recipe) {
+    this.order = new Order(data);
   }
 
+  close(): void {
+    this.dialogRef.close();
+  }
+
+  save(): void {
+    this.dialogRef.close(this.order);
+  }
 }
