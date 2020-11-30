4270592ae06e9e32e17b03df5d3e6470cacd6429
Pelat-1 <nicolo.rogai@outlook.com>
2020-11-30 16:51:10 +0100

    Each image now shows description and ingredients

--- src/app/menu-item/menu-item.component.ts
+++ src/app/menu-item/menu-item.component.ts
@@ -1,21 +1,33 @@
-import { Component, EventEmitter, Input, Output } from '@angular/core';
-import { Recipe } from '../shared/recipe';
+import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
+import { MatDialog, MatDialogConfig } from '@angular/material/dialog';
+import { ModalComponent } from '../modal/modal.component';
 import { Order } from '../shared/order';
+import { Recipe } from '../shared/recipe';
 
 @Component({
   selector: 'app-menu-item',
   templateUrl: './menu-item.component.html',
   styleUrls: ['./menu-item.component.css']
 })
-export class MenuItemComponent {
+export class MenuItemComponent implements OnInit {
   @Input()
   menuItem: Recipe;
-
   @Input()
   qty: number;
-
   @Output()
   ordering: EventEmitter<Order> = new EventEmitter();
+
+  private modalDialogConfig: MatDialogConfig<Recipe> = {
+    width: '30em',
+    height: '20em',
+    autoFocus: false
+  };
+
+  constructor(public dialog: MatDialog) { }
+
+  ngOnInit(): void {
+    this.modalDialogConfig.data = this.menuItem;
+  }
 
   plus1(): void {
     this.emit(new Order(this.menuItem, 1));
@@ -25,6 +37,15 @@ export class MenuItemComponent {
     if (this.qty > 0) {
       this.emit(new Order(this.menuItem, -1));
     }
+  }
+
+  openModalDialog(): void {
+    const modalDialogRef = this.dialog.open(ModalComponent, this.modalDialogConfig);
+    modalDialogRef.afterClosed().subscribe((result: Order) => {
+      if (result) {
+        console.log(JSON.stringify(result));
+      }
+    });
   }
 
   private emit(order: Order): void {
