3952ac3ea2332d87035375d403cabd1362d9c874
Pelat-1 <nicolo.rogai@outlook.com>
2020-12-05 15:23:21 +0100

    Auto-generated commit

--- deploy-zumi.bat
+++
@@ -1,2 +0,0 @@
-REM deploy zumi branch
-ng deploy --branch="master" --base-href="/Menu-Restaurant/zumi/"
--- src/app/shared/mock/mock-authors.ts
+++
@@ -1,9 +0,0 @@
-import { Author } from '../author';
-
-let index = 0;
-
-export const AUTHORS: Author[] = [
-  new Author(index++, 'Eucalyp'),
-  new Author(index++, 'Freepik'),
-  new Author(index++, 'mangsaabguru')
-].sort(Author.sort);
