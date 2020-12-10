79c900b85e3a5221016f89a7e26345e72060452e
Pelat-1 <nicolo.rogai@outlook.com>
2020-12-10 01:05:17 +0100

    Ingredients now has individual price for customizations

--- src/app/shared/ingredient.ts
+++ src/app/shared/ingredient.ts
@@ -3,14 +3,20 @@ import { Allergen } from './allergen';
 export class Ingredient {
   id: number;
   name: string;
-  noteName: string;
+  price: number;
   allergens: Allergen[];
+  noteName: string;
 
-  constructor(id: number, name: string, allergens: Allergen[] = [], noteName: string = name.toLowerCase()) {
+  constructor(id: number,
+              name: string,
+              price: number = 0.0,
+              allergens: Allergen[] = [],
+              noteName: string = name.toLowerCase()) {
     this.id = id;
     this.name = name;
-    this.noteName = noteName;
+    this.price = price;
     this.allergens = allergens.sort(Allergen.sort);
+    this.noteName = noteName;
   }
 
   public static sort(a: Ingredient, b: Ingredient): number {
