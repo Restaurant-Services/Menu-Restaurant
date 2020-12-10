e025d6023d7297582adf4e4690c6ad0a2abcc6bc
Pelat-1 <nicolo.rogai@outlook.com>
2020-12-11 00:35:44 +0100

    ModalOrder finished

--- src/app/modal/modal-order/modal-order.component.ts
+++ src/app/modal/modal-order/modal-order.component.ts
@@ -1,15 +1,26 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Inject } from '@angular/core';
+import { MAT_DIALOG_DATA, MatDialogRef } from '@angular/material/dialog';
+import { Order } from '../../shared/order';
 
 @Component({
   selector: 'app-modal-order',
   templateUrl: './modal-order.component.html',
   styleUrls: ['../styles.css', './modal-order.component.css']
 })
-export class ModalOrderComponent implements OnInit {
+export class ModalOrderComponent {
+  orders: Order[] = [];
 
-  constructor() { }
-
-  ngOnInit(): void {
+  constructor(public dialogRef: MatDialogRef<ModalOrderComponent>,
+              @Inject(MAT_DIALOG_DATA) public data: Order[]) {
+    data.forEach((order: Order) => {
+      if (order.quantity > 0) {
+        this.orders.push(order.clone());
+      }
+    });
+    this.orders.sort(Order.sort);
   }
 
+  close(): void {
+    this.dialogRef.close();
+  }
 }
