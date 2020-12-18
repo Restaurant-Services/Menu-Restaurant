5068b3165df958d2ab35a6bed1cdfb32659c0069
Pelat-1 <nicolo.rogai@outlook.com>
2020-12-18 14:34:31 +0100

    Fix id assign

--- src/app/_models/_mocks/mock-authors.ts
+++ src/app/_models/_mocks/mock-authors.ts
@@ -5,3 +5,7 @@ export const AUTHORS: Author[] = [
   new Author('Freepik'),
   new Author('mangsaabguru')
 ].sort(Author.sort);
+
+AUTHORS.forEach((author: Author, index: number) => {
+  author.id = index;
+});
